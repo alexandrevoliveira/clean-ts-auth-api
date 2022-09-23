43faecd44e5e56fca255f7a6a69bcc43f42a111e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-23 11:06:47 -0300

    test: ensure AddAccount calls HashGenerator with correct input

--- tests/domain/usecases/add-account.spec.ts
+++ tests/domain/usecases/add-account.spec.ts
@@ -1,3 +1,4 @@
+import { HashGenerator } from '@/domain/contracts/gateways'
 import { CheckUserAccountByEmail } from '@/domain/contracts/repos'
 import { AddAccount, setupAddAccount } from '@/domain/usecases'
 
@@ -5,22 +6,33 @@ import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('AddAccount', () => {
   let email: string
+  let password: string
   let userAccountRepo: MockProxy<CheckUserAccountByEmail>
+  let hasher: MockProxy<HashGenerator>
   let sut: AddAccount
 
   beforeAll(() => {
     email = 'any_email'
+    password = 'any_password'
     userAccountRepo = mock()
+    hasher = mock()
   })
 
   beforeEach(() => {
-    sut = setupAddAccount(userAccountRepo)
+    sut = setupAddAccount(userAccountRepo, hasher)
   })
 
   it('Should call CheckUserAccountByEmail with correct input', async () => {
-    await sut({ email })
+    await sut({ email, password })
 
     expect(userAccountRepo.checkByEmail).toHaveBeenCalledWith({ email })
     expect(userAccountRepo.checkByEmail).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call HashGenerator with correct input', async () => {
+    await sut({ email, password })
+
+    expect(hasher.generate).toHaveBeenCalledWith({ value: password })
+    expect(hasher.generate).toHaveBeenCalledTimes(1)
   })
 })
