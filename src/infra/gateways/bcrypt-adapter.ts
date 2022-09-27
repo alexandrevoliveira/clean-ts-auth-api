66e6fc30a060aac2f5523351f023e2b005536528
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-27 12:47:19 -0300

    feat: ensure BcryptAdapter returns a valid hash when generate succeeds

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -2,10 +2,10 @@ import { HashGenerator } from '@/domain/contracts/gateways'
 
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter {
+export class BcryptAdapter implements HashGenerator {
   constructor (private readonly salt: number) {}
 
-  async generate ({ value }: HashGenerator.Input): Promise<void> {
-    await bcrypt.hash(value, this.salt)
+  async generate ({ value }: HashGenerator.Input): Promise<HashGenerator.Output> {
+    return bcrypt.hash(value, this.salt)
   }
 }
