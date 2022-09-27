98877c9057c59069b43f07255a08030a51d581c2
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-27 12:48:15 -0300

    test: ensure BcryptAdapter returns a valid hash when generate succeeds

--- tests/infra/gateways/bcrypt-adapter.spec.ts
+++ tests/infra/gateways/bcrypt-adapter.spec.ts
@@ -5,13 +5,16 @@ import bcrypt from 'bcrypt'
 jest.mock('bcrypt')
 
 describe('BcryptAdapter', () => {
+  let value: string
   let salt: number
   let fakeBcrypt: jest.Mocked<typeof bcrypt>
   let sut: BcryptAdapter
 
   beforeAll(() => {
+    value = 'any_value'
     salt = 12
     fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>
+    fakeBcrypt.hash.mockImplementation(() => 'any_hashed_value')
   })
 
   beforeEach(() => {
@@ -19,8 +22,14 @@ describe('BcryptAdapter', () => {
   })
 
   it('should call hash with correct input', async () => {
-    await sut.generate({ value: 'any_value' })
+    await sut.generate({ value })
 
     expect(fakeBcrypt.hash).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a valid hash on hash success', async () => {
+    const hashedValue = await sut.generate({ value })
+
+    expect(hashedValue).toBe('any_hashed_value')
   })
 })
