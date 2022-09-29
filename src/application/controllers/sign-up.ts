d79d19208e227a8f783552e508e96b49eb6f54f1
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-28 23:37:56 -0300

    feat: ensure SignUpController calls AddAccount with correct input

--- src/application/controllers/sign-up.ts
+++ src/application/controllers/sign-up.ts
@@ -1,8 +1,19 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '../helpers'
+import { HttpResponse } from '@/application/helpers'
+import { AddAccount } from '@/domain/usecases'
+
+type HttpRequest = { name: string, email: string, password: string }
 
 export class SignUpController extends Controller {
-  async perform (httpRequest: any): Promise<HttpResponse<any>> {
-    throw new Error('Method not implemented.')
+  constructor (private readonly addAccount: AddAccount) {
+    super()
+  }
+
+  async perform ({ name, email, password }: HttpRequest): Promise<HttpResponse<any>> {
+    await this.addAccount({ name, email, password })
+    return {
+      statusCode: 400,
+      data: 'any_data'
+    }
   }
 }
