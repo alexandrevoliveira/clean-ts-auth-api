ef799b285ed917e7fc0442c5ef2c80e9d420c90e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-29 02:54:38 -0300

    feat: ensure Compare validates correctly if fields are objects

--- src/application/validation/compare.ts
+++ src/application/validation/compare.ts
@@ -9,6 +9,10 @@ export class Compare<T = string> implements Validator {
   ) {}
 
   validate (): Error | undefined {
-    if (this.field !== this.fieldToCompare) return new CompareFieldError(this.fieldToCompareName)
+    if (this.field instanceof Object && this.fieldToCompare instanceof Object) {
+      if (Object.values(this.field)[0] !== Object.values(this.fieldToCompare)[0]) return new CompareFieldError(this.fieldToCompareName)
+    } else {
+      if (this.field !== this.fieldToCompare) return new CompareFieldError(this.fieldToCompareName)
+    }
   }
 }
