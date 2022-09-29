9a3a2a1a413d1ec4da822cb5ad94302d8cd00318
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-28 23:38:51 -0300

    test: ensure SignUpController calls AddAccount with correct input

--- tests/application/controllers/sign-up.spec.ts
+++ tests/application/controllers/sign-up.spec.ts
@@ -1,13 +1,30 @@
 import { Controller, SignUpController } from '@/application/controllers'
 
 describe('SignUpController', () => {
+  let name: string
+  let email: string
+  let password: string
+  let addAccount: jest.Mock
   let sut: SignUpController
 
+  beforeAll(() => {
+    name = 'any_name'
+    email = 'any_email'
+    password = 'any_password'
+    addAccount = jest.fn()
+  })
+
   beforeEach(() => {
-    sut = new SignUpController()
+    sut = new SignUpController(addAccount)
   })
 
   it('should extend Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call AddAccount with correct input', async () => {
+    await sut.perform({ name, email, password })
+
+    expect(addAccount).toHaveBeenCalledWith({ name, email, password })
   })
 })
