5b0686344ecdee46a5a309868a43067fbf9c97af
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-10-20 13:28:17 -0300

    test: ensure Email returns undefined if validation succeeds

--- tests/application/validation/email.spec.ts
+++ tests/application/validation/email.spec.ts
@@ -3,10 +3,18 @@ import { EmailInvalidError } from '@/application/errors'
 
 describe('Email', () => {
   it('should return EmailInvalidError if validation fails', () => {
-    const sut = new Email()
+    const sut = new Email('alexandre-@mail.brasil.com')
 
     const error = sut.validate()
 
     expect(error).toEqual(new EmailInvalidError())
+  })
+
+  it('should return undefined if validation succeeds', () => {
+    const sut = new Email('1john1.1doe1@1my-company1.com')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
