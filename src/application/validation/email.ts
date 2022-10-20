30812b22ebf885888b4b511bfba3dbbd1b2273cf
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-10-20 13:27:16 -0300

    feat: ensure Email returns undefined if validation succeeds

--- src/application/validation/email.ts
+++ src/application/validation/email.ts
@@ -1,7 +1,22 @@
 import { EmailInvalidError } from '@/application/errors'
+import { Validator } from '@/application/validation'
 
-export class Email {
-  validate (): Error {
-    return new EmailInvalidError()
+export class Email implements Validator {
+  constructor (private readonly email: string) {}
+
+  validate (): Error | undefined {
+    const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    // RegExp
+    // ^ ==> should start with something
+    // $ ==> should end with it
+    // n+ ==> 1 or more occurrences of n
+    // () ==> group idea
+    // [] ==> find any character between the brackets
+    // n? ==> optional usage
+    // n* ==> 0 or more occurrences of n
+    // {2,3} ==> 2 or 3 items
+    if (this.email.match(mailFormat) === null) {
+      return new EmailInvalidError()
+    }
   }
 }
