5b0e731d0971ed144b80ed7d5836dd2565299349
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-10-26 00:47:54 -0300

    feat: ensure SignUpController builds validators correctly

--- src/application/controllers/sign-up.ts
+++ src/application/controllers/sign-up.ts
@@ -1,8 +1,9 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { AddAccount } from '@/domain/usecases'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 
-type HttpRequest = { name: string, email: string, password: string }
+type HttpRequest = { name: string, email: string, password: string, passwordConfirmation: string }
 
 export class SignUpController extends Controller {
   constructor (private readonly addAccount: AddAccount) {
@@ -15,5 +16,17 @@ export class SignUpController extends Controller {
       statusCode: 400,
       data: 'any_data'
     }
+  }
+
+  override buildValidators ({ name, email, password, passwordConfirmation }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: name, fieldName: 'name' }).required().build(),
+      ...Builder.of({ value: email, fieldName: 'email' }).required().email().build(),
+      ...Builder.of({ value: password, fieldName: 'password' })
+        .required()
+        .compare({ valueToCompare: passwordConfirmation, valueToCompareName: 'passwordConfirmation' })
+        .build(),
+      ...Builder.of({ value: passwordConfirmation, fieldName: 'passwordConfirmation' }).required().build()
+    ]
   }
 }
