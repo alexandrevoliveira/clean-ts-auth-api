01c25d71e7105fbb0b7c7f767b8333903aaebd04
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-10-26 00:49:49 -0300

    test: ensure SignUpController builds validators correctly

--- tests/application/controllers/sign-up.spec.ts
+++ tests/application/controllers/sign-up.spec.ts
@@ -1,9 +1,11 @@
 import { Controller, SignUpController } from '@/application/controllers'
+import { Compare, Email, RequiredString } from '@/application/validation'
 
 describe('SignUpController', () => {
   let name: string
   let email: string
   let password: string
+  let passwordConfirmation: string
   let addAccount: jest.Mock
   let sut: SignUpController
 
@@ -11,6 +13,7 @@ describe('SignUpController', () => {
     name = 'any_name'
     email = 'any_email'
     password = 'any_password'
+    passwordConfirmation = 'any_password'
     addAccount = jest.fn()
   })
 
@@ -23,8 +26,21 @@ describe('SignUpController', () => {
   })
 
   it('should call AddAccount with correct input', async () => {
-    await sut.perform({ name, email, password })
+    await sut.perform({ name, email, password, passwordConfirmation })
 
     expect(addAccount).toHaveBeenCalledWith({ name, email, password })
+  })
+
+  it('should build validators correctly', async () => {
+    const validators = sut.buildValidators({ name, email, password, passwordConfirmation })
+
+    expect(validators).toEqual([
+      new RequiredString('any_name', 'name'),
+      new RequiredString('any_email', 'email'),
+      new Email('any_email'),
+      new RequiredString('any_password', 'password'),
+      new Compare('any_password', 'any_password', 'passwordConfirmation'),
+      new RequiredString('any_password', 'passwordConfirmation')
+    ])
   })
 })
