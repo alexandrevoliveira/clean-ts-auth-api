46bca5035e5403098cd7a1663f950ecdbfe8205c
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-12-01 01:06:22 -0300

    feat: ensure signup route returns 200 with valid data

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,8 +1,9 @@
 import { adaptExpressRoute as adapt } from '@/main/adapters'
-import { makeFacebookLoginController } from '@/main/factories/application/controllers'
+import { makeFacebookLoginController, makeSignUpController } from '@/main/factories/application/controllers'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/login/facebook', adapt(makeFacebookLoginController()))
+  router.post('/signup', adapt(makeSignUpController()))
 }
