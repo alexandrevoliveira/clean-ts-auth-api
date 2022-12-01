a71cbcd640108199d3fe08838373a5848a25293d
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-12-01 11:21:14 -0300

    fix: ensure the field value is validated correctly

--- src/application/validation/builder.ts
+++ src/application/validation/builder.ts
@@ -18,7 +18,7 @@ export class ValidationBuilder {
       this.validators.push(new RequiredString(this.value, this.fieldName))
     } else {
       this.validators.push(new Required(this.value, this.fieldName))
-      if (this.value.buffer !== undefined) {
+      if (this.value?.buffer !== undefined) {
         this.validators.push(new RequiredBuffer(this.value.buffer, this.fieldName))
       }
     }
